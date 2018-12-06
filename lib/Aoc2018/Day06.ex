efeed2c0ff4cada1e72b1813a921f240911fdf5c
Kevin Hughes <kev@ksph.io>
2018-12-06 16:12:49 +0000

    Day Six

--- lib/Aoc2018/Day06.ex
+++ lib/Aoc2018/Day06.ex
@@ -1,9 +1,74 @@
 defmodule Aoc2018.Day06 do
-  def part_one(_input) do
-    :incomplete
+  def part_one(input) do
+    map = create_map(input)
+    [[min_x, max_x], [min_y, max_y]] = map_boundaries(map)
+
+    map =
+      for x <- min_x..max_x do
+        for y <- min_y..max_y, do: closest_loc(map, map, x, y)
+      end
+      |> List.flatten
+
+    infinite = Enum.filter(map, fn {x, y, _, t} ->
+      t == nil && (x == min_x || x == max_x || y == min_y || y == max_y)
+    end)
+    |> Enum.map(fn x -> elem(x,2) end)
+    |> MapSet.new
+
+    map
+    |> Enum.filter(fn {_, _, l, _} -> !MapSet.member?(infinite, l) end)
+    |> Enum.group_by(fn {_, _, l, _} -> l end)
+    |> Enum.map(fn {_, v} -> Enum.count(v) end)
+    |> Enum.sort
+    |> Enum.reverse
+    |> List.first
   end
 
-  def part_two(_input) do
-    :incomplete
+  def part_two(input, region \\ 10000) do
+    map = create_map(input)
+    [[min_x, max_x], [min_y, max_y]] = map_boundaries(map)
+    for x <- min_x..max_x do
+      for y <- min_y..max_y, do: total_distance(  map, x, y)
+    end |> List.flatten |> Enum.count(fn x -> x < region end)
+  end
+
+  def closest_loc(map, locations, x, y) do
+    cond do
+      loc = Enum.find(map, fn {lx, ly, _, _} -> {lx, ly} == {x,y} end) -> loc
+      true ->
+        [a, b | _] = Enum.map(locations, fn loc -> distance(loc, x, y) end)
+        |> Enum.sort_by(fn {_, dist} -> dist end)
+        case {a, b} do
+          {{_, dist}, {_, dist}} -> {x, y, nil, nil}
+          {{l, _}, _} -> {x, y, l, nil}
+        end
+    end
+  end
+
+  def create_map(input) do
+    input
+    |> String.trim_trailing("\n")
+    |> String.split("\n")
+    |> Enum.with_index
+    |> Enum.map(fn {s, i} ->
+      {x, y} = s |> String.split(", ") |> List.to_tuple
+      {elem(Integer.parse(x), 0), elem(Integer.parse(y), 0), i, :location}
+    end)
+  end
+
+  def map_boundaries(map) do
+    map
+    |> Enum.map(fn {x, y, _, _} -> [x, y] end)
+    |> Enum.zip
+    |> Enum.map(fn x -> x = Tuple.to_list(x); [Enum.min(x), Enum.max(x)] end)
+  end
+
+  def distance({x1, y1, n, :location}, x2, y2), do: {n, abs(x1-x2) + abs(y1-y2)}
+
+  def total_distance(locations, x, y) do
+    locations
+    |> Enum.map(fn l -> distance(l, x, y) end)
+    |> Enum.map(fn x -> elem(x, 1) end)
+    |> Enum.sum
   end
 end
--- test/Aoc2018/Day06_test.exs
+++ test/Aoc2018/Day06_test.exs
@@ -1,11 +1,13 @@
 defmodule Aoc2018.Day06Test do
 	use ExUnit.Case
 
+	@input "1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9"
+
 	test "Part One" do
-		assert Aoc2018.Day06.part_one(nil) == :incomplete
+		assert Aoc2018.Day06.part_one(@input) == 17
 	end
 
 	test "Part Two" do
-		assert Aoc2018.Day06.part_two(nil) == :incomplete
+		assert Aoc2018.Day06.part_two(@input, 32) == 16
 	end
 end
