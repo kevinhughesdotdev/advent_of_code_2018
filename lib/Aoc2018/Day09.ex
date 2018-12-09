805a9535676b3224019498a3d77ae152db58fecf
Kevin Hughes <kev@ksph.io>
2018-12-09 18:22:48 +0000

    Day Nine

--- lib/Aoc2018/Day09.ex
+++ lib/Aoc2018/Day09.ex
@@ -1,6 +1,47 @@
 defmodule Aoc2018.Day09 do
-  def part_one(_input) do
-    :incomplete
+  def part_one(input) do
+    [players, _, _, _, _, _, last, _] =
+      input
+      |> String.split
+
+    players = List.duplicate(0, String.to_integer(players)) |> List.to_tuple
+
+    scores = play([0], 1, 0, players, String.to_integer(last))
+  end
+
+  def part_two(input) do
+    [players, _, _, _, _, _, last, _] =
+      input
+      |> String.split
+
+    players = List.duplicate(0, String.to_integer(players)) |> List.to_tuple
+
+    scores = play([0], 1, 0, players, String.to_integer(last) * 100)
+  end
+
+  def play(circle, round, current, players, last) do
+    player = rem(round, tuple_size(players))
+    a = cond do
+      round == last ->
+        IO.inspect players
+        IO.puts players |> Tuple.to_list |> Enum.max
+      rem(round, 23) == 0 ->
+        IO.inspect [{round, players |> Tuple.to_list |> Enum.max}]
+        players = put_elem(players, player, elem(players, player) + round)
+        new_index = rem(Enum.find_index(circle, fn x -> current == x end) + length(circle) - 7, length(circle))
+        players = put_elem(players, player, elem(players, player) + Enum.at(circle, new_index))
+        {left, [_| right]} = Enum.split(circle, new_index)
+        circle = left ++ right
+        play(circle, round + 1, List.first(right), players, last)
+      true ->
+        new_index = rem(
+          Enum.find_index(circle, fn x -> current == x end) + 2,
+          length(circle))
+        {left, right} = Enum.split(circle, new_index)
+        new_circle = left ++ [round] ++ right
+        play(new_circle, round + 1, round, players, last)
+        player = rem(round, tuple_size(players))
+    end
   end
 
   def part_two(_input) do
--- test/Aoc2018/Day09_test.exs
+++ test/Aoc2018/Day09_test.exs
@@ -1,11 +1,19 @@
 defmodule Aoc2018.Day09Test do
 	use ExUnit.Case
 
+	@input1 "9 players; last marble is worth 25 points"
+	# @input1 "428 players; last marble is worth 72061 points"
+
 	test "Part One" do
-		assert Aoc2018.Day09.part_one(nil) == :incomplete
+		assert Aoc2018.Day09.part_one(@input1) == 32
+		# assert Aoc2018.Day09.part_one(10) == 8317
+		# assert Aoc2018.Day09.part_one(13) == 146373
+		# assert Aoc2018.Day09.part_one(17) == 2764
+		# assert Aoc2018.Day09.part_one(21) == 54718
+		# assert Aoc2018.Day09.part_one(30) == 37305
 	end
 
 	test "Part Two" do
-		assert Aoc2018.Day09.part_two(nil) == :incomplete
+		# assert Aoc2018.Day09.part_two(nil) == :incomplete
 	end
 end
