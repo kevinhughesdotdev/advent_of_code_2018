12617a88950bde39d944473f370322b18012aaf7
Kevin Hughes <kev@ksph.io>
2018-12-09 18:22:31 +0000

    Day Eight

--- lib/Aoc2018/Day08.ex
+++ lib/Aoc2018/Day08.ex
@@ -1,9 +1,58 @@
 defmodule Aoc2018.Day08 do
-  def part_one(_input) do
-    :incomplete
+
+  def part_one(input) do
+      input
+      |> String.split(" ")
+      |> Enum.map(&String.to_integer/1)
+      |> build_node
+      |> sum_meta
   end
 
-  def part_two(_input) do
-    :incomplete
+  def index_sum({[], meta}) do
+    Enum.sum(meta)
+  end
+
+  def index_sum({children,meta}) do
+    index_sums = Enum.map(children, &index_sum/1)
+    sums =
+      for index <- meta,
+        sum=Enum.at(index_sums, index - 1),
+        do: sum
+
+    Enum.sum(sums)
+  end
+
+  def sum_meta([h|l]) do
+    sum_meta(h) + sum_meta(l)
+  end
+
+  def sum_meta([]), do: 0
+
+  def sum_meta({a,b}) do
+    Enum.sum(b) + sum_meta(a)
+  end
+
+  def build_node([children_count, metadata_count | tail]) do
+    {children, tail} = children(children_count, tail, [])
+    {meta, tail} = Enum.split(tail, metadata_count)
+    {{children, meta}, tail}
+  end
+
+  def children(0, tail, acc) do
+    {Enum.reverse(acc), tail}
+  end
+
+  def children(count, tail, acc) do
+    {node, tail} = build_node(tail)
+    children(count - 1, tail, [node | acc])
+  end
+
+  def part_two(input) do
+    {root, _} = input
+    |> String.split(" ")
+    |> Enum.map(&String.to_integer/1)
+    |> build_node
+
+    root |> index_sum
   end
 end
--- test/Aoc2018/Day08_test.exs
+++ test/Aoc2018/Day08_test.exs
@@ -1,11 +1,13 @@
 defmodule Aoc2018.Day08Test do
 	use ExUnit.Case
 
+	@input "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+
 	test "Part One" do
-		assert Aoc2018.Day08.part_one(nil) == :incomplete
+		assert Aoc2018.Day08.part_one(@input) == 138
 	end
 
 	test "Part Two" do
-		assert Aoc2018.Day08.part_two(nil) == :incomplete
+		assert Aoc2018.Day08.part_two(@input) == 66
 	end
 end
