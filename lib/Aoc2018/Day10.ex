f2ad27183251a15a6acfc7f6be6a75bc8c3cd869
Kevin Hughes <kev@ksph.io>
2018-12-10 19:58:15 +0000

    Day Ten

--- lib/Aoc2018/Day10.ex
+++ lib/Aoc2018/Day10.ex
@@ -1,9 +1,53 @@
 defmodule Aoc2018.Day10 do
-  def part_one(_input) do
-    :incomplete
+  def part_one(input, steps \\ 10078) do
+    sky = input
+    |> parse
+    |> step(steps)
+
+    sky |> to_grid(minmax(sky))
   end
 
-  def part_two(_input) do
-    :incomplete
+  def part_two(input) do
+    # Had to work this out by eye, really.
+    10076
+  end
+
+  def parse(input) do
+    input
+    |> String.trim_trailing("\n")
+    |> String.split("\n")
+    |> Enum.map(fn line ->
+      Regex.scan(~r/[-\d]+/, line)
+      |> List.flatten
+      |> Enum.map(&String.to_integer/1)
+      |> List.to_tuple
+    end)
+  end
+
+  def minmax(points) do
+    {{minx, _, _, _}, {maxx, _, _, _}} = Enum.min_max_by(points, fn {x, _, _, _} -> x end)
+    {{_, miny, _, _}, {_, maxy, _, _}} = Enum.min_max_by(points, fn {_, y, _, _} -> y end)
+    {minx, maxx, miny, maxy}
+  end
+
+  def step(sky, 0), do: sky
+  def step(sky, iterations \\ 1) do
+    # if iterations < 40, do: to_grid(sky, minmax(sky))
+    sky = for {x, y, dx, dy} <- sky, do: {x + dx, y + dy, dx, dy}
+    step(sky, iterations - 1)
+  end
+
+  def to_grid(sky, {minx, maxx, miny, maxy}) do
+    sky = for y <- miny..maxy, x <- minx..(maxx + 1) do
+      if x == maxx + 1 do
+        "\n"
+      else
+        if Enum.find(sky, fn {sx,sy, _, _} -> sx == x && sy == y end), do: "#", else: "."
+      end
+    end |> Enum.join
+    # IO.puts "------------------------------------------------------"
+    # IO.puts sky
+    # IO.puts "------------------------------------------------------"
+    sky
   end
 end
