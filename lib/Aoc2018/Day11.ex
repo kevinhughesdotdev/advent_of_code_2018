5059f808872e067d69b83e3c9d21769b2a4be81f
Kevin Hughes <kev@ksph.io>
2018-12-11 08:10:50 +0000

    Day 11 Part One

--- lib/Aoc2018/Day11.ex
+++ lib/Aoc2018/Day11.ex
@@ -1,6 +1,29 @@
 defmodule Aoc2018.Day11 do
-  def part_one(_input) do
-    :incomplete
+  def part_one(input) do
+    serial = String.to_integer(input)
+    squares = for y <- 1..498, x <- 1..498, do: {x, y, square_power(x, y, serial)}
+    {x, y, power} = Enum.max_by(squares, &elem(&1, 2))
+    "#{x},#{y}"
+  end
+
+  def power_level(x, y, serial) do
+    (
+      rackID = x + 10
+      (((y * rackID) + serial) * rackID)
+      |> Integer.to_string
+      |> String.slice(-3, 1)
+      |> String.to_integer
+    ) - 5
+  end
+
+  def square_coords(x, y) do
+    for y <- y..(y + 2), x <- x..(x + 2), do: {x, y}
+  end
+
+  def square_power(x, y, serial) do
+    (for {x, y} <- square_coords(x, y), do: power_level(x, y, serial))
+    # |> IO.inspect
+    |> Enum.sum
   end
 
   def part_two(_input) do
--- test/Aoc2018/Day11_test.exs
+++ test/Aoc2018/Day11_test.exs
@@ -2,7 +2,22 @@ defmodule Aoc2018.Day11Test do
 	use ExUnit.Case
 
 	test "Part One" do
-		assert Aoc2018.Day11.part_one(nil) == :incomplete
+		assert Aoc2018.Day11.part_one("5468") == "493,64"
+	end
+
+	test "power level" do
+		assert Aoc2018.Day11.power_level(3,5,8) == 4
+		assert Aoc2018.Day11.power_level(122, 79, 57) == -5
+		assert Aoc2018.Day11.power_level(217, 196, 39) == 0
+		assert Aoc2018.Day11.power_level(101, 153, 71) == 4
+	end
+
+	test "square coords" do
+		assert Aoc2018.Day11.square_coords(1,1) == [
+			{1, 1}, {2, 1}, {3, 1},
+			{1, 2}, {2, 2}, {3, 2},
+			{1, 3}, {2, 3}, {3, 3},
+		]
 	end
 
 	test "Part Two" do
