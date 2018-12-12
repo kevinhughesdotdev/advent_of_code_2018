797fffa1c5c516549288ba31cfd5c6ec61b7bce1
Kevin Hughes <kev@ksph.io>
2018-12-12 09:19:47 +0000

    Went back and refactored Day 2 a little

--- lib/Aoc2018/Day02.ex
+++ lib/Aoc2018/Day02.ex
@@ -2,34 +2,24 @@ defmodule Aoc2018.Day02 do
   def part_one(input) do
     input = input
       |> String.split("\n")
-      |> Enum.map(fn(x) -> checksum(x) end)
-    counts = {
-      Enum.count(input, fn({x, _}) -> x end),
-      Enum.count(input, fn({_, x}) -> x end)
-    }
-    elem(counts, 0) * elem(counts, 1)
+      |> Enum.map(&checksum(&1))
+    Enum.count(input, fn({x, _}) -> x end) *
+    Enum.count(input, fn({_, x}) -> x end)
   end
 
   def part_two(input) do
     list = input
+      |> String.trim_trailing("\n")
       |> String.split("\n")
-      |> Enum.reject(fn x -> x == "" end)
-    matches = for x <- list do
-      for y <- list do
-        difference(x,y)
-      end
-    end
-    matches
-      |> List.flatten
-      |> Enum.find(fn x -> x end)
+    (for x <- list, y <- list, do: difference(x,y)) |> Enum.find(&(&1))
   end
 
   defp checksum(box_id) do
     counts = box_id
       |> String.split("")
-      |> Enum.group_by(fn(x) -> x end)
+      |> Enum.group_by(&(&1))
       |> Map.delete("")
-      |> Enum.map(fn {k, v} -> {k,Enum.count(v)} end)
+      |> Enum.map(fn {k, v} -> { k, Enum.count(v) } end)
       |> Map.new
     {
       Enum.find(counts, fn {_, v} -> v == 2 end) && 1,
