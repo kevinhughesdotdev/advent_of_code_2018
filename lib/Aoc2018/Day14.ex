8f292c6c5ad485ce7f650d625fa84285688923fd
Kevin Hughes <kev@ksph.io>
2018-12-15 08:30:45 +0000

    Day 14 - too slow

--- lib/Aoc2018/Day14.ex
+++ lib/Aoc2018/Day14.ex
@@ -1,9 +1,55 @@
 defmodule Aoc2018.Day14 do
-  def part_one(_input) do
-    :incomplete
+  def part_one(input) do
+    input = String.to_integer(input)
+
+    {recipes, _, _} = new_recipes([3, 7], 0, 1, 0, input + 10)
+
+    recipes
+    |> Enum.split(input)
+    |> elem(1)
+    |> Enum.split(10)
+    |> elem(0)
+    |> Enum.join()
   end
 
-  def part_two(_input) do
-    :incomplete
+  def new_recipes(recipes, elf1, elf2, max, max), do: {recipes, elf1, elf2}
+
+  def new_recipes(recipes, elf1, elf2, count, max) do
+    elf1r = Enum.at(recipes, elf1)
+    elf2r = Enum.at(recipes, elf2)
+
+    new_recipes =
+      (elf1r + elf2r)
+      |> Integer.to_string()
+      |> String.split("")
+      |> Enum.reject(fn x -> x == "" end)
+      |> Enum.map(&String.to_integer(&1))
+
+    recipes = recipes ++ new_recipes
+    elf1 = rem(elf1 + elf1r + 1, length(recipes))
+    elf2 = rem(elf2 + elf2r + 1, length(recipes))
+
+    new_recipes(recipes, elf1, elf2, count + 1, max)
+  end
+
+  def find_string(recipe, string, elf1, elf2) do
+    if Enum.random(0..100) == 43, do: IO.puts(length(recipe))
+
+    cond do
+      String.ends_with?(Enum.join(recipe), string) ->
+        Regex.compile(string)
+        |> elem(1)
+        |> Regex.run(Enum.join(recipe), return: :index)
+        |> List.first()
+        |> elem(0)
+
+      true ->
+        {recipe, elf1, elf2} = new_recipes(recipe, elf1, elf2, 0, 1)
+        find_string(recipe, string, elf1, elf2)
+    end
+  end
+
+  def part_two(input) do
+    find_string([3, 7], input, 0, 1)
   end
 end
--- test/Aoc2018/Day14_test.exs
+++ test/Aoc2018/Day14_test.exs
@@ -1,11 +1,24 @@
 defmodule Aoc2018.Day14Test do
   use ExUnit.Case
+  alias Aoc2018.Day14
 
   test "Part One" do
-    assert Aoc2018.Day14.part_one(nil) == :incomplete
+    assert Aoc2018.Day14.part_one("9") == "5158916779"
+    assert Aoc2018.Day14.part_one("5") == "0124515891"
+    assert Aoc2018.Day14.part_one("18") == "9251071085"
+    assert Aoc2018.Day14.part_one("2018") == "5941429882"
+  end
+
+  test "new recipes" do
+    assert Day14.new_recipes([3, 7], 0, 1, 0, 1) == {[3, 7, 1, 0], 0, 1}
+    assert Day14.new_recipes([3, 7, 1, 0], 0, 1, 0, 1) == {[3, 7, 1, 0, 1, 0], 4, 3}
+    assert Day14.new_recipes([3, 7, 1, 0, 1, 0], 4, 3, 0, 1) == {[3, 7, 1, 0, 1, 0, 1], 6, 4}
   end
 
   test "Part Two" do
-    assert Aoc2018.Day14.part_two(nil) == :incomplete
+    assert Aoc2018.Day14.part_two("51589") == 9
+    assert Aoc2018.Day14.part_two("01245") == 5
+    assert Aoc2018.Day14.part_two("92510") == 18
+    assert Aoc2018.Day14.part_two("59414") == 2018
   end
 end
