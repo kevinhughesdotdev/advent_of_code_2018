048c2669a6e50f0e546b1b95854bbbcbdcf430ba
Kevin Hughes <kev@ksph.io>
2019-11-21 15:04:58 +0000

    Tests for d1p1

--- ruby/test/aoc2018/day01_test.rb
+++ ruby/test/aoc2018/day01_test.rb
@@ -4,7 +4,10 @@ require_relative '../../lib/aoc2018/day01.rb'
 module Aoc2018::Test
   class Day01 < MiniTest::Test
     def test_part_one
-      assert_equal Aoc2018::Day01.part_one(nil), :incomplete
+      assert_equal Aoc2018::Day01.part_one("+1\n-2\n+3\n+1"), 3
+      assert_equal Aoc2018::Day01.part_one("+1\n+1\n+1"), 3
+      assert_equal Aoc2018::Day01.part_one("+1\n+1\n-2"), 0
+      assert_equal Aoc2018::Day01.part_one("-1\n-2\n-3"), -6
     end
 
     def test_part_two
