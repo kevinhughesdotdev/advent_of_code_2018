53acd2f43be19d067e50fe97ae7d701ed5087969
Kevin Hughes <kev@ksph.io>
2019-12-01 10:54:06 +0000

    Bit of refactoring

--- 2019/ruby/lib/aoc2019/day01.rb
+++ 2019/ruby/lib/aoc2019/day01.rb
@@ -5,20 +5,18 @@ module Aoc2019
     end
 
     def self.part_one(input)
-      input.split.map{ |x| line(x.to_i) }.sum
+      input.split.map { |x| line(x.to_i) }.sum
     end
 
     def self.line2(mass, sum = 0)
-      if line(mass) <= 0
-        return sum
-      else
-        new_mass = line(mass)
-        return line2(new_mass, sum + new_mass)
-      end
+      new_mass = line(mass)
+      return sum if new_mass <= 0
+
+      line2(new_mass, sum + new_mass)
     end
 
     def self.part_two(input)
-      input.split.map{ |x| line2(x.to_i) }.sum
+      input.split.map { |x| line2(x.to_i) }.sum
     end
   end
 end
