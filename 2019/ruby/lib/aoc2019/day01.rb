9543117703955dbde92da1210af09a6550d1c033
Kevin Hughes <kev@ksph.io>
2019-12-01 10:38:31 +0000

    Ruby day 01 part one

--- 2019/ruby/lib/aoc2019/day01.rb
+++ 2019/ruby/lib/aoc2019/day01.rb
@@ -1,7 +1,11 @@
 module Aoc2019
   class Day01
-    def self.part_one(_input)
-      :incomplete
+    def self.line(input)
+      (input / 3).round(half: :down) - 2
+    end
+
+    def self.part_one(input)
+      input.split.map{ |x| line(x.to_i) }.sum
     end
 
     def self.part_two(_input)
--- 2019/ruby/test/aoc2019/day01_test.rb
+++ 2019/ruby/test/aoc2019/day01_test.rb
@@ -3,12 +3,19 @@ require_relative '../../lib/aoc2019/day01.rb'
 
 module Aoc2019::Test
   class Day01 < MiniTest::Test
+    def part_one_line
+      assert_equal 2, Aoc2019::Day01.line(12)
+      assert_equal 2, Aoc2019::Day01.line(14)
+      assert_equal 654, Aoc2019::Day01.line(1969)
+      assert_equal 33583, Aoc2019::Day01.line(100756)
+    end
+
     def test_part_one
-      assert_equal Aoc2019::Day01.part_one(nil), :incomplete
+      assert_equal 34241, Aoc2019::Day01.part_one("12\n14\n1969\n100756\n")
     end
 
     def test_part_two
-      assert_equal Aoc2019::Day01.part_one(nil), :incomplete
+      assert_equal Aoc2019::Day01.part_two(nil), :incomplete
     end
   end
 end
