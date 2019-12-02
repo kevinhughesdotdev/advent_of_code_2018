157f3aae6e69a69da6bcbbefe1cd374db9985878
Kevin Hughes <kev@ksph.io>
2019-12-02 09:35:09 +0000

    Ruby Day 02

--- 2019/ruby/lib/aoc2019/day02.rb
+++ 2019/ruby/lib/aoc2019/day02.rb
@@ -1,11 +1,49 @@
 module Aoc2019
   class Day02
-    def self.part_one(_input)
-      :incomplete
+    class Program
+      attr_accessor :state, :p, :finished
+
+      def initialize(*state)
+        @state = state
+        @finished = false
+        @p = 0
+      end
+
+      def step
+        case state[@p]
+        when 1 then state[state[@p + 3]] = state[state[@p + 1]] + state[state[@p + 2]]
+        when 2 then state[state[@p + 3]] = state[state[@p + 1]] * state[state[@p + 2]]
+        when 99 then @finished = true
+        else raise "Unknown opcode! #{@p}: #{@state}"
+        end
+        @p += 4
+      end
+
+      def run
+        self.step until @finished
+        self
+      end
     end
 
-    def self.part_two(_input)
-      :incomplete
+    def self.part_one(input)
+      program = Program.new(*input.split(",").map(&:to_i))
+      program.state[1] = 12
+      program.state[2] = 2
+      program.run.state[0]
+    end
+
+    def self.part_two(input)
+      catch(:complete) do
+        0.upto(99) do |noun|
+          0.upto(99) do |verb|
+            program = Program.new(*input.split(",").map(&:to_i))
+            program.state[1] = noun
+            program.state[2] = verb
+            program.run.state[0]
+            throw(:complete, 100 * noun + verb) if program.state[0] == 19690720
+          end
+        end
+      end
     end
   end
 end
--- 2019/ruby/test/aoc2019/day02_test.rb
+++ 2019/ruby/test/aoc2019/day02_test.rb
@@ -3,12 +3,46 @@ require_relative '../../lib/aoc2019/day02.rb'
 
 module Aoc2019::Test
   class Day02 < MiniTest::Test
-    def test_part_one
-      assert_equal Aoc2019::Day02.part_one(nil), :incomplete
+    def test_run
+      assert_equal [2,0,0,0,99], Aoc2019::Day02::Program.new(1,0,0,0,99).run.state
+      assert_equal [2,3,0,6,99], Aoc2019::Day02::Program.new(2,3,0,3,99).run.state
+      assert_equal [2,4,4,5,99,9801], Aoc2019::Day02::Program.new(2,4,4,5,99,0).run.state
+      assert_equal [30,1,1,4,2,5,6,0,99], Aoc2019::Day02::Program.new(1,1,1,4,99,5,6,0,99).run.state
+    end
+
+    def test_new_prog
+      program = Aoc2019::Day02::Program.new(1,9,10,3,2,3,11,0,99,30,40,50)
+      assert_equal [1,9,10,3,2,3,11,0,99,30,40,50], program.state
+      assert_equal 0, program.p
+    end
+
+    def test_addition
+      program = Aoc2019::Day02::Program.new(1,9,10,3,2,3,11,0,99,30,40,50)
+      program.step
+      assert_equal [1,9,10,70,2,3,11,0,99,30,40,50], program.state
+      assert_equal 4, program.p
+      refute program.finished
+    end
+
+    def test_multiplication
+      program = Aoc2019::Day02::Program.new(1,9,10,70,2,3,11,0,99,30,40,50)
+      program.p = 4
+      program.step
+      assert_equal [3500,9,10,70,2,3,11,0,99,30,40,50], program.state
+      assert_equal 8, program.p
+      refute program.finished
+    end
+
+    def test_stop
+      program = Aoc2019::Day02::Program.new(3500,9,10,70,2,3,11,0,99,30,40,50)
+      program.p = 8
+      program.step
+      assert_equal [3500,9,10,70,2,3,11,0,99,30,40,50], program.state
+      assert program.finished
     end
 
     def test_part_two
-      assert_equal Aoc2019::Day02.part_one(nil), :incomplete
+      assert_equal Aoc2019::Day02.part_two(nil), :incomplete
     end
   end
 end
