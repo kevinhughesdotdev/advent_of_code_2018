7fc792092f31dee91edd6be70e6b8c70365b4f9d
Kevin Hughes <kev@ksph.io>
2019-12-03 09:24:53 +0000

    Day one part one Elixir

--- 2019/elixir/lib/Aoc2019/Day03.ex
+++ 2019/elixir/lib/Aoc2019/Day03.ex
@@ -1,9 +1,48 @@
 defmodule Aoc2019.Day03 do
-	def part_one(_input) do
-		:incomplete
-	end
+  def path(x, y, "L" <> count) do
+    count = String.to_integer(count)
+    (x - 1)..(x - count) |> Enum.map(fn xs -> {xs, y} end)
+  end
 
-	def part_two(_input) do
-		:incomplete
-	end
+  def path(x, y, "R" <> count) do
+    count = String.to_integer(count)
+    (x + 1)..(x + count) |> Enum.map(fn xs -> {xs, y} end)
+  end
+
+  def path(x, y, "D" <> count) do
+    count = String.to_integer(count)
+    (y - 1)..(y - count) |> Enum.map(fn ys -> {x, ys} end)
+  end
+
+  def path(x, y, "U" <> count) do
+    count = String.to_integer(count)
+    (y + 1)..(y + count) |> Enum.map(fn ys -> {x, ys} end)
+  end
+
+  def path(wire), do: path(wire, 0, 0)
+
+  def path([], _, _, coords), do: coords
+
+  def path([h | t], x, y, coords \\ []) do
+    new_coords = path(x, y, h)
+    {newx, newy} = List.last(new_coords)
+    path(t, newx, newy, new_coords ++ coords)
+  end
+
+  def part_one(input) do
+    coords =
+      input
+      |> String.split("\n")
+      |> Enum.map(fn w -> String.split(w, ",") end)
+      |> Enum.map(&path/1)
+      |> Enum.map(&MapSet.new/1)
+
+    apply(MapSet, :intersection, coords)
+    |> Enum.map(fn {x, y} -> abs(x) + abs(y) end)
+    |> Enum.min()
+  end
+
+  def part_two(_input) do
+    :incomplete
+  end
 end
--- 2019/elixir/test/Aoc2019/Day03_test.exs
+++ 2019/elixir/test/Aoc2019/Day03_test.exs
@@ -1,11 +1,27 @@
 defmodule Aoc2019.Day03Test do
-	use ExUnit.Case
+  use ExUnit.Case
 
-	test "Part One" do
-		assert Aoc2019.Day03.part_one(nil) == :incomplete
-	end
+  test "coords for a wire path" do
+    assert Aoc2019.Day03.path(0, 0, "R8") == [
+             {1, 0},
+             {2, 0},
+             {3, 0},
+             {4, 0},
+             {5, 0},
+             {6, 0},
+             {7, 0},
+             {8, 0}
+           ]
 
-	test "Part Two" do
-		assert Aoc2019.Day03.part_two(nil) == :incomplete
-	end
+    assert Aoc2019.Day03.path(6, 0, "D5") == [{6, -1}, {6, -2}, {6, -3}, {6, -4}, {6, -5}]
+  end
+
+  # 
+  # test "Part One" do
+  #   assert Aoc2019.Day03.part_one(nil) == :incomplete
+  # end
+
+  test "Part Two" do
+    assert Aoc2019.Day03.part_two(nil) == :incomplete
+  end
 end
